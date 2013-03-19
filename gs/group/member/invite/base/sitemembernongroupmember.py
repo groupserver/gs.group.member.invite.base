a2ad095bb1a7f2a5f5cbff2e5c38bbcc50ae3a6f
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-20 09:33:43 +1100

    Cleanup of the SiteMemberNonGroupMember class.

--- gs/group/member/invite/base/sitemembernongroupmember.py
+++ gs/group/member/invite/base/sitemembernongroupmember.py
@@ -5,10 +5,11 @@ from zope.component import createObject
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.interfaces import IVocabulary, \
   IVocabularyTokenized, ITitledTokenizedTerm
-from zope.interface.common.mapping import IEnumerableMapping 
-from gs.group.member.base.utils import user_member_of_group
+from zope.interface.common.mapping import IEnumerableMapping
+from gs.group.member.base import user_member_of_group
 from gs.profile.email.base.emailuser import EmailUser
 from gs.site.member import SiteMembers
+
 
 class SiteMembersNonGroupMembers(object):
     implements(IVocabulary, IVocabularyTokenized)
@@ -16,7 +17,7 @@ class SiteMembersNonGroupMembers(object):
 
     def __init__(self, context):
         self.context = context
-                
+
     @Lazy
     def acl_users(self):
         sr = self.context.site_root()
@@ -28,49 +29,65 @@ class SiteMembersNonGroupMembers(object):
     def siteInfo(self):
         retval = createObject('groupserver.SiteInfo', self.context)
         return retval
-    
+
     @Lazy
     def groupsInfo(self):
         retval = createObject('groupserver.GroupsInfo', self.context)
         return retval
-    
+
     @Lazy
     def groupInfo(self):
         retval = createObject('groupserver.GroupInfo', self.context)
         return retval
-        
+
     @Lazy
     def siteMembers(self):
         return SiteMembers(self.context)
+
+    def has_addr(self, ui):
+        return bool(EmailUser(self.context, ui).get_delivery_addresses())
 
     @Lazy
     def nonMembers(self):
         '''Get the members of the current site that are not a member of
         the group, and who have an email address.'''
-        groupMemberGroupId = '%s_member' % self.groupInfo.id
-        retval = [ui for ui in self.siteMembers.members
+        # OPTIMIZE: This could *mostly* be done with lists of IDs
+        retval = [EmailUser(self.context, ui)
+                    for ui in self.siteMembers.members
                     if ((not user_member_of_group(ui, self.groupInfo))
-                        and (EmailUser(self.context, ui).get_addresses()))]
+                        and self.has_addr(ui))]
         assert type(retval) == list
+        return retval
+
+    @Lazy
+    def nonMemberIds(self):
+        retval = [m.userInfo.id for m in self.nonMembers]
+        return retval
+
+    def get_display_name(self, emailUser):
+        userInfo = emailUser.userInfo
+        addr = emailUser.get_delivery_addresses()[0]
+        r = u'{0} (<code class="email">{1}</code>)'
+        retval = r.format(userInfo.name, addr)
         return retval
 
     def __iter__(self):
         """See zope.schema.interfaces.IIterableVocabulary"""
-        retval = [SimpleTerm(m.id, m.id, m.name)
-                  for m in self.nonMembers]
-        for term in retval:
+        for m in self.nonMembers:
+            uid = m.userInfo.id
+            term = SimpleTerm(uid, uid, self.get_display_name(m))
             assert term
             assert ITitledTokenizedTerm in providedBy(term)
             assert term.token == term.value
-        return iter(retval)
+            yield(term)
 
     def __len__(self):
         """See zope.schema.interfaces.IIterableVocabulary"""
-        return len(self.groups)
+        return len(self.nonMembers)
 
     def __contains__(self, value):
         """See zope.schema.interfaces.IBaseVocabulary"""
-        retval = value in [m.id for m in self.nonMembers]
+        retval = value in self.nonMemberIds
         assert type(retval) == bool
         return retval
 
@@ -81,15 +98,15 @@ class SiteMembersNonGroupMembers(object):
     def getTerm(self, value):
         """See zope.schema.interfaces.IBaseVocabulary"""
         return self.getTermByToken(value)
-        
+
     def getTermByToken(self, token):
         """See zope.schema.interfaces.IVocabularyTokenized"""
         for m in self.nonMembers:
-            if m.id == token:
-                retval = SimpleTerm(m.id, m.id, m.name)
+            uid = m.userInfo.id
+            if uid == token:
+                retval = SimpleTerm(uid, uid, self.get_display_name(m))
                 assert retval
                 assert ITitledTokenizedTerm in providedBy(retval)
                 assert retval.token == retval.value
                 return retval
-        raise LookupError, token
-
+        raise LookupError(token)
