9c095c45fb6ac57046cf0932f0d80afda5099a7b
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-20 09:34:28 +1100

    Updating the egg deps

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -7,7 +7,7 @@ version = get_version()
 
 setup(name='gs.group.member.invite.base',
     version=version,
-    description="The pages that are required to invite people to "\
+    description="The pages that are required to invite people to "
       "join GroupServer groups.",
     long_description=open("README.txt").read() + "\n" +
                     open(os.path.join("docs", "HISTORY.txt")).read(),
@@ -28,25 +28,40 @@ setup(name='gs.group.member.invite.base',
     url='http://groupserver.org',
     license='ZPL',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.member', 'gs.group.member.invite'],
+    namespace_packages=['gs', 'gs.group', 'gs.group.member',
+                        'gs.group.member.invite'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'setuptools',
+        'pytz',
+        'sqlalchemy',
+        'zope.app.apidoc',
+        'zope.cachedescriptors',
+        'zope.component',
+        'zope.contentprovider',
+        'zope.formlib',
+        'zope.interface',
+        'zope.pagetemplate',
+        'zope.publisher',
+        'zope.schema',
+        'zope.sqlalchemy',
+        'Zope2',
         'gs.content.form',
+        'gs.content.layout',
         'gs.database',
+        'gs.group.base',
         'gs.group.member.base',
         'gs.help',
         'gs.profile.email.base',
         'gs.profile.notify',
         'gs.site.member',
+        'Products.GSAuditTrail',
         'Products.GSProfile',
         'Products.XWFCore',
         'Products.CustomUserFolder'
-        # -*- Extra requirements: -*-
     ],
     entry_points="""
     # -*- Entry points: -*-
     """,
 )
-
