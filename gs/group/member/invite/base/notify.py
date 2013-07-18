15bd56a1de2adde2b8d642bd7f15fbd1a9ee2f9d
Michael JasonSmith <mpj17@onlinegroups.net>
2013-07-17 17:00:32 -0700

    Fixing the content-type header when sending the notifications.

--- gs/group/member/invite/base/notify.py
+++ gs/group/member/invite/base/notify.py
@@ -1,24 +1,25 @@
 # coding=utf-8
-from textwrap import TextWrapper
 from zope.component import createObject, getMultiAdapter
 from zope.cachedescriptors.property import Lazy
 from gs.profile.notify.sender import MessageSender
 UTF8 = 'utf-8'
 
+
 class InvitationNotifier(object):
     textTemplateName = 'invitationmessage.txt'
     htmlTemplateName = 'invitationmessage.html'
-    
+
     def __init__(self, context, request):
         self.context = context
         self.request = request
+        self.oldContentType = self.request.response.getHeader('Content-Type')
 
     @Lazy
     def groupInfo(self):
         retval = createObject('groupserver.GroupInfo', self.context)
         assert retval, 'Could not create the GroupInfo from %s' % self.context
         return retval
-        
+
     @Lazy
     def loggedInUserInfo(self):
         retval = createObject('groupserver.LoggedInUser', self.context)
@@ -28,36 +29,29 @@ class InvitationNotifier(object):
 
     @Lazy
     def textTemplate(self):
-        retval = getMultiAdapter((self.context, self.request), 
+        retval = getMultiAdapter((self.context, self.request),
                     name=self.textTemplateName)
         assert retval
         return retval
 
     @Lazy
     def htmlTemplate(self):
-        retval = getMultiAdapter((self.context, self.request), 
+        retval = getMultiAdapter((self.context, self.request),
                     name=self.htmlTemplateName)
         assert retval
         return retval
-               
+
     def notify(self, adminInfo, userInfo, fromAddr, toAddrs,
                 invitationId, subject, message):
         assert toAddrs, 'No to address for %s' % userInfo.name
-        text = self.textTemplate(   adminInfo       = adminInfo,
-                                    userInfo        = userInfo,
-                                    fromAddr        = fromAddr,
-                                    toAddr          = toAddrs[0],
-                                    subject         = subject,
-                                    invitationId    = invitationId,
-                                    message         = message)
-        html = self.htmlTemplate(   adminInfo       = adminInfo,
-                                    userInfo        = userInfo,
-                                    fromAddr        = fromAddr,
-                                    toAddr          = toAddrs[0],
-                                    subject         = subject,
-                                    invitationId    = invitationId,
-                                    message         = message,
-                                    fakeHeader      = False)
+        text = self.textTemplate(adminInfo=adminInfo, userInfo=userInfo,
+                                    fromAddr=fromAddr, toAddr=toAddrs[0],
+                                    subject=subject, invitationId=invitationId,
+                                    message=message)
+        html = self.htmlTemplate(adminInfo=adminInfo, userInfo=userInfo,
+                                    fromAddr=fromAddr, toAddr=toAddrs[0],
+                                    subject=subject, invitationId=invitationId,
+                                    message=message, fakeHeader=False)
         ms = MessageSender(self.context, userInfo)
         ms.send_message(subject, text, html, fromAddr, toAddrs)
-
+        self.request.response.setHeader('Content-Type', self.oldContentType)
