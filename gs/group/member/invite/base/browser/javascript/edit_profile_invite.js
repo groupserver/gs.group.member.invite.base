4d43b7045dfaad735ff6a8c7fc4bc9b0ecba1b56
Michael JasonSmith <mpj17@onlinegroups.net>
2014-01-27 10:14:29 -0800

    Upping the version number.

--- gs/group/member/invite/base/browser/javascript/edit_profile_invite.js
+++ gs/group/member/invite/base/browser/javascript/edit_profile_invite.js
@@ -1,3 +1,13 @@
+// Copyright © 2014 OnlineGroups.net and Contributors.
+// All Rights Reserved.
+//
+// This software is subject to the provisions of the Zope Public License,
+// Version 2.1 (ZPL). http://groupserver.org/downloads/license/
+//
+// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+// WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+// FITNESS FOR A PARTICULAR PURPOSE.
 jQuery.noConflict()
 
 function popupPreview (event) {
